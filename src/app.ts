dcb2ba57dcbdf014f7f3c1d3b1eaa4f3839f4119
arifuddincoder <arifuddincoder@gmail.com>
2026-01-23 21:59:28 +0600

    added post modules with routes controller and services

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application } from "express";
+import { postRouter } from "./modules/post/post.router";
+
+const app: Application = express();
+app.use(express.json());
+app.use("/posts", postRouter);
+
+app.get("/", (req, res) => {
+	res.send("Hii, This is arif");
+});
+
+export default app;
